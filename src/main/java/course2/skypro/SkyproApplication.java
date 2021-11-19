f388bbefeeb194b5fe16de1475f586572a681f74
i_kap <iryna.kapelka@gmail.com>
2021-11-19 19:50:21 +0300

    Initial commit

---
+++ src/main/java/course2/skypro/SkyproApplication.java
@@ -0,0 +1,13 @@
+package course2.skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproApplication.class, args);
+	}
+
+}
